14aee1ecf508235af6c7d0fe8fbd908a08914cf6
bikash-bk <brandedbikash@gmail.com>
2026-04-16 18:57:45 +0530

    middleware to handle cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 const express = require("express")
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+
+app.use(cors({
+    origin: process.env.FRONT_END_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+)
 
 
 app.listen(3000,(()=>{
