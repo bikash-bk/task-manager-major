b5cdd898070b23444cc09b1ab5c2d7a0f7c3636f
bikash-bk <brandedbikash@gmail.com>
2026-04-16 17:44:16 +0530

    tailwind css setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">Bikash</div>
+    <div className="text-3xl font-bold underline">app</div>
   )
 }
 
